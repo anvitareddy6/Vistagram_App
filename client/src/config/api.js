4d5eb6a465bc598e31e181fc0f79884cf6b2102c
anvitareddy6 <anvitareddy6@gmail.com>
2025-09-29 17:43:11 +0530

    Railway Production Liks Added

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -2,7 +2,7 @@
 const isDevelopment = process.env.NODE_ENV === 'development';
 
 export const API_URL = process.env.REACT_APP_API_URL || 
-  (isDevelopment ? 'http://localhost:5000/api' : 'https://vistagramapp-production.up.railway.app/');
+  (isDevelopment ? 'http://localhost:5000/api' : 'https://vistagramapp-production.up.railway.app/api');
 
 export const SERVER_URL = process.env.REACT_APP_SERVER_URL || 
-  (isDevelopment ? 'http://localhost:5000' : 'https://vistagramapp-production.up.railway.app/s');
+  (isDevelopment ? 'http://localhost:5000' : 'https://vistagramapp-production.up.railway.app/');
