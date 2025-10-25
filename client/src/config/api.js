b29bf3c060d34c8fb7b7dad36fed89af40e68f53
Anvita Reddy <anvita@Anvitas-MacBook-Pro.local>
2025-10-25 19:06:07 +0530

    Vercel Changes made

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -1,6 +1,7 @@
 // API Configuration
 // const isDevelopment = process.env.NODE_ENV === 'development';
 
-export const API_URL = 'https://vistagramapp-production-c497.up.railway.app/api';
-
-export const SERVER_URL = 'https://vistagramapp-production-c497.up.railway.app';
+// export const API_URL = 'https://vistagramapp-production-c497.up.railway.app/api';
+export const API_URL = 'http://localhost:8080/api';
+// export const SERVER_URL = 'https://vistagramapp-production-c497.up.railway.app';
+export const SERVER_URL = 'http://localhost:8080';
